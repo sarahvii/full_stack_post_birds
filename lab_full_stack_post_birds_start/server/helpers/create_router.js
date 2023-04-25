a9b903bb0c53503a76e398a3e7cd0bb2b57e2470
Sarah Smith <sarahsmithvii@gmail.com>
2023-04-25 15:36:29 +0100

    mvp

---
+++ lab_full_stack_post_birds_start/server/helpers/create_router.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { ObjectId } = require('mongodb');
+
+const createRouter = function (collection) {
+
+  const router = express.Router();
+
+
+  //INDEX
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  // SHOW
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: new ObjectId(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  // POST
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+      .insertOne(newData)
+      .then(result => {
+        res.json({ _id: result.insertedId})
+      // .catch((err) => {
+      //   console.error(err);
+      //   res.status(500);
+      //   res.json({ status: 500, error: err });
+      //  });
+      });
+  });
+
+  // DESTROY
+    router.delete('/:id/delete', (req, res) => {
+      const id = req.params.id;
+      collection
+        .deleteOne({ _id: new ObjectId(id)})
+        .then(result => res.json (result))
+        .catch((err) => {
+          console.error(err);
+          res.status(500);
+          res.json({ status: 500, error: err});
+        });
+  });
+  
+
+  return router;
+};
+
+module.exports = createRouter;
